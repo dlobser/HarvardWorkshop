32108e0dba105f5e9014a4b5c2d103bb105a0cfa
david lobser <davidlobser@yahoo.com>
2019-10-17 11:39:15 -0400

    added in class assignment

---
+++ Assets/InClassAssignment/Thing.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Thing : MonoBehaviour
+{
+    /* Create variables to determine the behavior of this thing
+     * such as a vector for moving
+     * a vector for eulerangles (rotating)
+     * an age
+     * a lifespan
+     */
+
+    void Start()
+    {
+        /* When the object is created, consider randomly setting some of your variables
+         * this will help create variety
+         */
+    }
+
+    void Update()
+    {
+        /* transform your object, move it in some direction 
+         * (randomly, toward the camera, out from the center)
+         * animate other variables as it travels (optional)
+         * add Time.deltaTime to your age variable
+         * if age is greater than lifespan, destroy this object
+         * GameObject.Destroy(this.gameObject);
+         */
+    }
+}
---
+++ Assets/InClassAssignment/ThingManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* Create a system that will generate objects which can be interacted with
+ * The system should spawn new objects every 'n' seconds
+ * The generated objects should destroy themselves when they get old
+ * 
+ * Examples: Dance Dance Revolution, Beat Saber, Asteroids, Space Invaders
+ * 
+ * Optional: 
+ *      The objects create explosion effects when they're destroyed
+ *      The objects scale up from zero when they're born and scale down when they die
+ *      Different kinds of objects with different behaviors can be created
+ *      When the objects are destroyed they spawn new, smaller objects
+ */
+
+public class ThingManager : MonoBehaviour
+{
+    
+    /* Define a public variable for your instantiatable 'Thing' (public GameObject or public Thing)
+     * Define an amount variable to limit the number of things which will be created
+     * Define variables to define the initial placement of the things
+     * Define a timer variable to set when a new object will spawn
+     * Define a frequency variable to set how high the timer will climb
+     * Define an amountOfThings variable to keep track of how many there are (optional)
+     * Define a 'List' variable to keep track of the objects (optional)
+     */
+
+    void Start()
+    {
+        // Create your new list here (optional)
+    }
+
+    void Update()
+    {
+        /* use your timer variable to count up until it is larger than your frequency variable (Time.deltaTime)
+         * set your timer to 0
+         * if amountOfThings is less amount, instantiate a new thing
+         * add 1 to amountOfThings (optional)
+         * add that thing to your list (optional)
+         * parent that thing to a container, such as this.transform (optional)
+         */
+    }
+}
